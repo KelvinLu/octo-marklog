58b961ec06778c997f3722468bd22e688a0ca049
Kelvin Lu <klu@ucdavis.edu>
2014-10-01 16:39:28 -0700

    Front page almost finished

--- marklog/static/js/listings.js
+++ marklog/static/js/listings.js
@@ -1,16 +1,25 @@
 $( document ).ready(function() {
 	var $container = $('#listings');
 
-	$container.masonry({
-		itemSelector: '.listing',
+	$container.imagesLoaded(function(){
+		$container.masonry({
+			itemSelector: '.listing',
+		});
 	});
 
 	$container.infinitescroll({
 		nextSelector: "div#pagination a:last",
   		navSelector: "div#pagination",
   		itemSelector: "div.listing",
+  		loading: {
+  			finishedMsg: "",
+  		}
 	}, function(newElements) {
     	var $newElems = $(newElements);
-    	$container.masonry('appended', $newElems);
+    	$newElems.css({opacity: 0,});
+		$newElems.imagesLoaded(function(){
+			$newElems.animate({opacity: 1,}, 100);
+    		$container.masonry('appended', $newElems);
+		});    	
   	});
 });
