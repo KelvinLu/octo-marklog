c325ce6049f77ed9d0ee07aaa5cf5f77d20f1338
Kelvin Lu <klu@ucdavis.edu>
2014-10-04 00:02:21 -0700

    added lorem ipsum generator script

--- marklog/assets/posts/fakeposts.py
+++ marklog/assets/posts/fakeposts.py
@@ -1,18 +1,32 @@
-import time
+import sys
+import urllib2
+import random
 
-fakemd = """Title: My {0}th Post
-PreviewText: This is my {0}th post!
-PreviewImage: http://confrazzled.com/wp-content/uploads/2014/08/cat2.jpg
+# tybg for restful-like apis
+md_url = r'http://jaspervdj.be/lorem-markdownum/markdown.txt'
+text_url = r'http://loripsum.net/api/plaintext/short/1'
+image_url = r'http://hhhhold.com/l'
 
-An h1 header
-============
+try:
+	i = int(sys.argv[1])
+except Exception:
+	i = 1
 
-This is post {0}
-"""
+for j in range(i):
+	md = urllib2.urlopen(md_url).read()
 
-for i in range(4, 30):
-	print("Making my_" + str(i) + "th_post.md")
-	f = open("my_" + str(i) + "th_post.md", 'w')
-	f.write(fakemd.format(str(i)))
+	meta = {
+		"title": urllib2.urlopen(text_url).read()[57:].split(".")[0],
+		"date": "2014-" + str(random.randint(1, 12)) + "-" + str(random.randint(1, 28)),
+		"previewtext": urllib2.urlopen(text_url).read()[57:].strip(),
+		"previewimage": image_url if random.choice([True, False]) else None,
+	}
+
+	f = open("fakepost_" + str(random.randint(1000, 9999)) + '.md', 'w')
+
+	for k, v in meta.items():
+		if v is not None:
+			f.write(k + ": " + v + "\n")
+
+	f.write('\n' + md)
 	f.close()
-	time.sleep(61)
